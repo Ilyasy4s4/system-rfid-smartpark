fe4515dc85f70b823ab6d10ac02aceb12a6baaae
ilyasy4s4 <ilyasgumalar@gmail.com>
2026-04-20 06:34:22 +0100

    first

---
+++ src/main/java/com/mycompany/smartparkid/SmartparkId.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.smartparkid;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class SmartparkId {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
