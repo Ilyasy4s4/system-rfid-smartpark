8f88b3d2e5ab3aa7a91e3a15184b9fbaf60bed87
ilyas <ilyasgumalar@gmail.com>
2026-04-27 11:36:40 +0700

    database

--- src/main/java/objects/MongoManager.java
+++ src/main/java/objects/MongoManager.java
@@ -9,9 +9,32 @@ package objects;
  * @author LENOVO
  */
 
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.bson.codecs.configuration.CodecRegistries;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+
 
 
 public class MongoManager {
-
-   
+    private static MongoClient mongoClient;
+    private static final String DATABASE_NAME = "SmartPark_db";
+    
+    public static MongoDatabase getDatabase() {
+        if (mongoClient == null) {
+            CodecRegistry pojoCodecRegistry = CodecRegistries.fromRegistries(
+                    MongoClientSettings.getDefaultCodecRegistry(),
+                    codecRegistries.fromProviders(PojoCodecProvider.builder().automatic(true).build())
+            );
+            
+            mongoClient =MongoClient.create("mongodb://localhost:27017");
+            
+            return mongoClient.getDatabase(DATABASE_NAME).withCodecRegistry(pojoCodecRegistry);
+        }
+        return mongoClient.getDatabase(DATABASE_NAME);
+    }
+     
 }
