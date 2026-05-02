28665e7e128d1f68e0c1e863d057cbfc28808ad7
ilyasy4s4 <ilyasgumalar@gmail.com>
2026-05-02 03:25:22 +0100

    integrasi mongodb

--- src/main/java/objects/MongoManager.java
+++ src/main/java/objects/MongoManager.java
@@ -2,43 +2,43 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package objects;
+
 
 /**
  *
  * @author LENOVO
  */
 
+package objects;
+
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClients; // Import yang benar
 import com.mongodb.client.MongoDatabase;
 import org.bson.codecs.configuration.CodecRegistries;
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
 
-
-
 public class MongoManager {
-<<<<<<< HEAD
+    
     private static MongoClient mongoClient;
     private static final String DATABASE_NAME = "SmartPark_db";
     
     public static MongoDatabase getDatabase() {
         if (mongoClient == null) {
+            // PERBAIKAN 1: Gunakan 'CodecRegistries' (C Besar) bukan huruf kecil
             CodecRegistry pojoCodecRegistry = CodecRegistries.fromRegistries(
                     MongoClientSettings.getDefaultCodecRegistry(),
-                    codecRegistries.fromProviders(PojoCodecProvider.builder().automatic(true).build())
+                    CodecRegistries.fromProviders(PojoCodecProvider.builder().automatic(true).build())
             );
             
-            mongoClient =MongoClient.create("mongodb://localhost:27017");
+            // PERBAIKAN 2: Gunakan 'MongoClients.create' (pakai S) 
+            // Dan sebaiknya masukkan pengaturan koneksi ke dalam settings
+            mongoClient = MongoClients.create("mongodb://localhost:27017");
             
+            // PERBAIKAN 3: Pastikan database dipanggil dengan registry yang baru dibuat
             return mongoClient.getDatabase(DATABASE_NAME).withCodecRegistry(pojoCodecRegistry);
         }
         return mongoClient.getDatabase(DATABASE_NAME);
     }
-     
-=======
-   
->>>>>>> 15a078a788bf2e970914265c5528d9bb02809ee0
 }
