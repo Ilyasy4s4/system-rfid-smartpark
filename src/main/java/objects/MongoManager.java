e1b248ca4875a629e9bb639d7e70e712984360b7
ilyasy4s4 <ilyasgumalar@gmail.com>
2026-05-02 03:29:31 +0100

    integrasi database mongodb

--- src/main/java/objects/MongoManager.java
+++ src/main/java/objects/MongoManager.java
@@ -13,7 +13,7 @@ package objects;
 
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients; // Import yang benar
+import com.mongodb.client.MongoClients; 
 import com.mongodb.client.MongoDatabase;
 import org.bson.codecs.configuration.CodecRegistries;
 import org.bson.codecs.configuration.CodecRegistry;
@@ -26,17 +26,16 @@ public class MongoManager {
     
     public static MongoDatabase getDatabase() {
         if (mongoClient == null) {
-            // PERBAIKAN 1: Gunakan 'CodecRegistries' (C Besar) bukan huruf kecil
+            
             CodecRegistry pojoCodecRegistry = CodecRegistries.fromRegistries(
                     MongoClientSettings.getDefaultCodecRegistry(),
                     CodecRegistries.fromProviders(PojoCodecProvider.builder().automatic(true).build())
             );
             
-            // PERBAIKAN 2: Gunakan 'MongoClients.create' (pakai S) 
-            // Dan sebaiknya masukkan pengaturan koneksi ke dalam settings
+            
             mongoClient = MongoClients.create("mongodb://localhost:27017");
             
-            // PERBAIKAN 3: Pastikan database dipanggil dengan registry yang baru dibuat
+            
             return mongoClient.getDatabase(DATABASE_NAME).withCodecRegistry(pojoCodecRegistry);
         }
         return mongoClient.getDatabase(DATABASE_NAME);
